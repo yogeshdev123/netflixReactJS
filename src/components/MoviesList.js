91e696c598738ac246c1776fbe0982f2cda64a85
yogeshdev123 <samuelyogesh37@gmail.com>
2025-03-06 11:04:09 +0530

    added css file to hide the scrollbar

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,12 +1,12 @@
 import MovieCard from "./MovieCard"
-
+import "./scrollbar.css";
 const MoviesList = ({title,movies}) => {
     // console.log(movies);
     if(movies==null)return;
   return (
     <div className="px-6">
             <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
-    <div className="flex overflow-x-scroll ">
+    <div className="scrollBar flex overflow-x-scroll ">
         
        
     <div className="flex">
