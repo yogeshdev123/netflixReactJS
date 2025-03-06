54af5f325b581374fe155590be38cd3f7221b545
yogeshdev123 <samuelyogesh37@gmail.com>
2025-03-06 10:50:01 +0530

    refresh button added to error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,13 @@
 
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Error = () => {
+    const navigate=useNavigate();
   return (
+    <>
     <div>Switch to mobile network if u encounter this message</div>
+    <button onClick={()=>{navigate("/")}}>Refresh</button></>
   )
 }
 
